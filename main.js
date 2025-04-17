ee804a2df51d5b741bf8d6f0ec777f006babcac0
destiny503 <503destiny503@gmail.com>
2025-04-17 17:03:27 +0400

    massive update

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// игра
-
 if (window.screen.width < 300) {
   alert("Неподдерживаемое устройство. Складные устройства надо раскрыть😏");
 }
@@ -8,23 +6,6 @@ let perfect = document.querySelector(".perfect");
 let car = document.querySelector(".car");
 let wheel = document.querySelector(".wheel");
 let wheelFront = document.querySelector(".front");
-let scWheels = false;
-let scSpoilers = false;
-let scColors = false;
-let scSprings = false;
-
-let checkScore = setInterval(() => {
-  console.log(scWheels, scSpoilers, scColors, scSprings)
-  if (scWheels && scSpoilers && scColors && scSprings) {
-    perfect.style.visibility = "visible";
-    clearInterval(checkScore);
-    wheel.style.animation = "wheel-anim 2s infinite linear";
-    wheelFront.style.animation = "wheel-anim 2s infinite linear";
-    car.style.animation = "5s linear forwards drive";
-  }
-}, 1000);
-
-// меню
 
 let allMenu = document.querySelectorAll(".menu");
 let mainMenu = document.querySelector(".menu.main");
@@ -33,156 +14,93 @@ let spoilersMenu = document.querySelector(".menu.spoilers");
 let colorsMenu = document.querySelector(".menu.colors");
 let springsMenu = document.querySelector(".menu.springs");
 
-let mainBtns = document.querySelectorAll(".main-btn");
-let wheelsBtn = document.querySelector(".wheels-btn");
-let spoilersBtn = document.querySelector(".spoilers-btn");
-let colorsBtn = document.querySelector(".colors-btn");
-let springsBtn = document.querySelector(".springs-btn");
+let carStatus = {
+  wheels: true,
+  spoilers: false,
+  colors: false,
+  springs: false,
+};
 
-function menuSwith() {
-  switch (this.flag) {
-    case 0:
-      allMenu.forEach((i) => {
-        i.style.visibility = "hidden";
-      });
-      mainMenu.style.visibility = "visible";
-      break;
-    case 1:
-      allMenu.forEach((i) => {
-        i.style.visibility = "hidden";
-      });
-      wheelsMenu.style.visibility = "visible";
-      break;
-    case 2:
-      allMenu.forEach((i) => {
-        i.style.visibility = "hidden";
-      });
-      spoilersMenu.style.visibility = "visible";
-      break;
-    case 3:
-      allMenu.forEach((i) => {
-        i.style.visibility = "hidden";
-      });
-      colorsMenu.style.visibility = "visible";
-      break;
-    case 4:
-      allMenu.forEach((i) => {
-        i.style.visibility = "hidden";
-      });
-      springsMenu.style.visibility = "visible";
-      break;
-    default:
-      alert("Ошибка переключения меню");
+// гейплей
+
+function isPerfect() {
+  if (Object.values(carStatus).every(Boolean)) {
+    perfect.style.visibility = "visible";
   }
 }
 
-mainBtns.forEach((btn) => {
-  btn.addEventListener("click", { handleEvent: menuSwith, flag: 0 });
+perfect.addEventListener('click', () => {
+  if (!perfect.classList.contains("grow-animation")) {
+    perfect.classList.add("grow-animation");
+  }
+  wheel.style.animation = "wheel-anim 2s infinite linear";
+  wheelFront.style.animation = "wheel-anim 2s infinite linear";
+  car.style.animation = "5s linear forwards drive";
 });
-wheelsBtn.addEventListener("click", { handleEvent: menuSwith, flag: 1 });
-spoilersBtn.addEventListener("click", { handleEvent: menuSwith, flag: 2 });
-colorsBtn.addEventListener("click", { handleEvent: menuSwith, flag: 3 });
-springsBtn.addEventListener("click", { handleEvent: menuSwith, flag: 4 });
 
-// колёса
+// меню
 
-let wheels = document.querySelectorAll(".wheel-img");
-let wheel1Btn = document.querySelector(".wheel1");
-let wheel2Btn = document.querySelector(".wheel2");
-let wheel3Btn = document.querySelector(".wheel3");
-
-function changeWheel1() {
-  wheels.forEach((wheel) => {
-    wheel.setAttribute("src", "./img/wheel1.png");
-    scWheels = false;
+function openMenuByName(menuName) {
+  allMenu.forEach(menu => {
+    menu.style.visibility = "hidden";
   });
+
+  const targetMenu = document.querySelector(`.menu.${menuName}`);
+  if (targetMenu) {
+    targetMenu.style.visibility = "visible";
+  } else {
+    console.warn(`Меню "${menuName}" не найдено`);
+  }
 }
 
-function changeWheel2() {
-  wheels.forEach((wheel) => {
-    wheel.setAttribute("src", "./img/wheel2.png");
-    scWheels = true;
+document.querySelectorAll('[data-menu]').forEach(button => {
+  button.addEventListener('click', () => {
+    const menuName = button.dataset.menu;
+    openMenuByName(menuName);
   });
+});
+
+document.querySelectorAll('.menu .button').forEach(button => {
+  const type = button.dataset.type;
+  const value = button.dataset.value;
+
+  if (type && value) {
+    button.addEventListener('click', () => handleChange(type, value));
+  }
+});
+
+// подменю
+
+function handleChange(type, value) {
+  switch (type) {
+    case 'wheel':
+      document.querySelectorAll('.wheel-img').forEach(w => {
+        w.src = `./img/wheel${value}.png`;
+      });
+      carStatus.wheels = value !== "1";
+      break;
+
+    case 'spoiler':
+      const spoiler = document.querySelector('.spoiler-img');
+      if (value === "1") {
+        spoiler.src = "./img/spoiler1.png";
+        carStatus.spoilers = false;
+      } else {
+        spoiler.src = `./img/spoiler${value}.png`;
+        carStatus.spoilers = true;
+      }
+      spoiler.style.height = (value === "3") ? "33px" :
+                             (value === "4") ? "50px" : "auto";
+      break;
+
+    case 'color':
+      document.querySelector('.color-img').src = `./img/priora${value}.png`;
+      carStatus.colors = value !== "1";
+      break;
+  }
+
+  isPerfect();
 }
-
-function changeWheel3() {
-  wheels.forEach((wheel) => {
-    wheel.setAttribute("src", "./img/wheel3.png");
-    scWheels = true;
-  });
-}
-
-wheel1Btn.addEventListener("click", changeWheel1);
-wheel2Btn.addEventListener("click", changeWheel2);
-wheel3Btn.addEventListener("click", changeWheel3);
-
-// спойлеры
-
-let spoiler = document.querySelector(".spoiler-img");
-let spoiler1Btn = document.querySelector(".spoiler1");
-let spoiler2Btn = document.querySelector(".spoiler2");
-let spoiler3Btn = document.querySelector(".spoiler3");
-let spoiler4Btn = document.querySelector(".spoiler4");
-
-function changeSpoiler1() {
-  spoiler.setAttribute("src", "./img/spoiler1.png");
-  scSpoilers = false;
-}
-
-function changeSpoiler2() {
-  spoiler.setAttribute("src", "./img/spoiler2.png");
-  scSpoilers = true;
-}
-
-function changeSpoiler3() {
-  spoiler.setAttribute("src", "./img/spoiler3.png");
-  spoiler.style.height = "33px";
-  scSpoilers = true;
-}
-
-function changeSpoiler4() {
-  spoiler.setAttribute("src", "./img/spoiler-4.png");
-  spoiler.style.height = "50px";
-  scSpoilers = true;
-}
-
-spoiler1Btn.addEventListener("click", changeSpoiler1);
-spoiler2Btn.addEventListener("click", changeSpoiler2);
-spoiler3Btn.addEventListener("click", changeSpoiler3);
-spoiler4Btn.addEventListener("click", changeSpoiler4);
-
-// цвета
-
-let color = document.querySelector(".color-img");
-let color1Btn = document.querySelector(".color1");
-let color2Btn = document.querySelector(".color2");
-let color3Btn = document.querySelector(".color3");
-let color4Btn = document.querySelector(".color4");
-
-function changeColor1() {
-  color.setAttribute("src", "./img/priora.png");
-  scColors = false;
-}
-
-function changeColor2() {
-  color.setAttribute("src", "./img/priora-yellow.png");
-  scColors = true;
-}
-
-function changeColor3() {
-  color.setAttribute("src", "./img/priora-pink.png");
-  scColors = true;
-}
-
-function changeColor4() {
-  color.setAttribute("src", "./img/priora-usa.png");
-  scColors = true;
-}
-
-color1Btn.addEventListener("click", changeColor1);
-color2Btn.addEventListener("click", changeColor2);
-color3Btn.addEventListener("click", changeColor3);
-color4Btn.addEventListener("click", changeColor4);
 
 // занижение
 
@@ -192,24 +110,21 @@ let springsSlider = document.querySelector("#springs-value");
 
 function changeSprings() {
   let width = +window.screen.width;
+  let val = +springsSlider.value;
 
-  if (springsSlider.value == 62 || springsSlider.value == 80) {
-    scSprings = true;
-  }
+  carStatus.springs = true;
 
   if (width >= 400) {
-    for (const spring of springs) {
-      spring.style.top = this.value + "px";
-    }
-    shadow.style.top = +this.value + 36 + "px";
+    springs.forEach(spring => spring.style.top = val + "px");
+    shadow.style.top = val + 36 + "px";
   } else if (width >= 300) {
-    for (const spring of springs) {
-      spring.style.top = +this.value - 15 + "px";
-    }
-    shadow.style.top = +this.value + 11 + "px";
+    springs.forEach(spring => spring.style.top = val - 15 + "px");
+    shadow.style.top = val + 11 + "px";
   } else {
     alert("Неподдерживаемое устройство. Складное надо раскрыть😏");
   }
+
+  isPerfect();
 }
 
 springsSlider.addEventListener("input", changeSprings);
